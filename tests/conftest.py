390255cd36abc614f47eb2953df65d1307319b2d
Alena Panova <aljonchik1905@mail.ru>
2026-01-24 13:55:32 +0800

    combining fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from utils import attach
 
 
 @pytest.fixture(scope="function")
-def setup_browser_t():
+def setup_browser_u():
     """Фикстура для настройки и управления браузером Chrome через Selene"""
 
     chrome_options = Options()
@@ -50,7 +50,6 @@ def setup_browser_t():
 
 @pytest.fixture(scope="function")
 def setup_browser():
-    """Фикстура для настройки и управления браузером Chrome через Selenoid"""
     load_dotenv(
         dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env")
     )
@@ -59,41 +58,67 @@ def setup_browser():
     selenoid_pass = os.getenv("SELENOID_PASS")
     selenoid_url = os.getenv("SELENOID_URL")
 
-    # Настройка capabilities для Selenoid
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "127.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True,
-            # ,
-            # "sessionTimeout": "5m",  # Увеличиваем до 5 минут
-            # "env": ["TZ=UTC"]
-        },
-    }
-    options.capabilities.update(selenoid_capabilities)
+    if selenoid_url:
+        # Настройка capabilities для Selenoid
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "127.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True,
+                # ,
+                # "sessionTimeout": "5m",  # Увеличиваем до 5 минут
+                # "env": ["TZ=UTC"]
+            },
+        }
+        options.capabilities.update(selenoid_capabilities)
 
-    # Создаем драйвер для Selenoid
-    driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-        options=options,
-    )
+        # Создаем драйвер для Selenoid
+        driver = webdriver.Remote(
+            command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+            options=options,
+        )
 
-    driver.set_window_size(1920, 1080)
+        driver.set_window_size(1920, 1080)
 
-    driver.execute_cdp_cmd(
-        "Page.addScriptToEvaluateOnNewDocument",
-        {
-            "source": """
-            window.localStorage.setItem('WELCOME_MODAL_DONT_SHOW', 'true');
-        """
-        },
-    )
+        driver.execute_cdp_cmd(
+            "Page.addScriptToEvaluateOnNewDocument",
+            {
+                "source": """
+                window.localStorage.setItem('WELCOME_MODAL_DONT_SHOW', 'true');
+            """
+            },
+        )
 
-    # Настройка Selene с созданным драйвером
-    browser.config.driver = driver
-    browser.config.timeout = 10
+        # Настройка Selene с созданным драйвером
+        browser.config.driver = driver
+        browser.config.timeout = 10
+    else:
+        """Фикстура для настройки и управления браузером Chrome через Selene"""
+
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
+
+        driver = webdriver.Chrome(options=chrome_options)
+
+        driver.execute_cdp_cmd(
+            "Page.addScriptToEvaluateOnNewDocument",
+            {
+                "source": """
+                window.localStorage.setItem('WELCOME_MODAL_DONT_SHOW', 'true');
+            """
+            },
+        )
+
+        browser.config.driver = driver
+
+        browser.config.base_url = "https://alpmap.ru"
+        browser.config.timeout = 10
+        browser.config.save_page_source_on_failure = False
 
     yield browser
 
